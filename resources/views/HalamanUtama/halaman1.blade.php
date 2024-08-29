e85a6f8f7007dc33bcac5721f378962f20371a80
tiara1990citra <tiara1990citra@gmail.com>
2024-08-29 15:18:57 +0700

    new

--- resources/views/HalamanUtama/halaman1.blade.php
+++ resources/views/HalamanUtama/halaman1.blade.php
@@ -9,7 +9,6 @@
         <div class="col-lg-6">
             <div class="card-body">
                 <p class="card-text">gambar buku</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic reprehenderit unde consectetur, dolor quos porro doloremque magni assumenda quidem! Deserunt, non. Asperiores deleniti, sint eos repellendus, dolores obcaecati vitae alias, perspiciatis totam magnam animi. Tempora ex beatae, nesciunt nostrum natus temporibus labore vitae asperiores blanditiis. Nemo, ad rem error harum ab, est ut pariatur esse perspiciatis explicabo debitis quasi cumque reprehenderit assumenda! Sed excepturi quisquam exercitationem a! A, corrupti debitis non animi nesciunt, itaque doloremque quas aspernatur maiores culpa atque? Eum impedit cupiditate facere! Culpa repellendus fuga doloribus incidunt assumenda.</p>
             </div>
             <div class="button">
                 <buttn type="button" class="btn btn-success">Dikembalikan</buttn>
